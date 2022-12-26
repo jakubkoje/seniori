fa14d34a151e52a2206e251e71d9562105e8a7e1
Jakub Jelínek <jakubkoje@gmail.com>
2022-12-26 19:52:41 +0100

    added tracking

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,10 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@inkline/nuxt'
+    '@inkline/nuxt',
+    'nuxt-umami'
   ],
   inkline: {
     size: 'lg'
+  },
+  umami: {
+    enable: true, // enable the module? true by default
+    autoTrack: true,
+    doNotTrack: false,
+    cache: false,
+    domains: 'smelo.pucwoll.com',
+    websiteId: '59f9b0ef-2693-4bab-b179-dc64182642ae',
+    scriptUrl: 'https://umami.pucwoll.com/umami.js'
   },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
